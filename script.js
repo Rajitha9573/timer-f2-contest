47fa238e7f5a4c75f01af81b1268d19d7059093c
Rajitha9573 <rajithabhukya967@gmail.com>
2023-09-30 20:53:19 +0530

    fdvf

---
+++ script.js
@@ -0,0 +1,17 @@
+//  let time = window.prseInt(prompt("Enter a value"));
+
+let timerElement = document.getElementById("btn");
+ let hourElement =document.getElementsById("hour");
+ inputElement.innertext=time;
+  timerElement.addEventListener("click", toggleElement);
+
+
+let currentTiming = document.getElementById("currentTiming");
+function toggleElement(){
+    
+   let hr= window.parseInt(prompt("Enter hour"));
+    window.parseInt(prompt("Enter minutes"));
+    window.parseInt(prompt("Enter seconds"));
+     
+}
+
